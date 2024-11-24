e05b99974c892accbf15cfa62037f4c46972efee
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-24 18:33:36 +0200

    Ex 3 Anagram

--- Anagram.java
+++ Anagram.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
 /** Functions for checking if a given string is an anagram. */
 public class Anagram {
 	public static void main(String args[]) {
@@ -28,22 +32,50 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		char[] charsS1 = new char[str1.length()];
+		char[] charsS2 = new char[str2.length()];
+
+		if(str1.length() != str2.length())
+			return false;
+		else
+		{
+			int num = str1.length();
+			for(int i = 0; i < num ; i++)
+			{
+				charsS1[i] = str1.charAt(i);
+				charsS2[i] = str2.charAt(i);
+			}
+			return Arrays.equals(charsS1,charsS2);
+		}
+
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		// str = str.replaceAll("^a-z]);
+		str = str.toLowerCase();
+		return str;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		str = preProcess(str);
+		char[] charsStr = new char[str.length()];
+		for(int i = 0; i < str.length() ; i++)
+				charsStr[i] = str.charAt(i);
+		Random rnd = new Random();
+		for(int i = str.length() - 1 ; i > 0 ; i--)
+		{
+			int index = rnd.nextInt(i + 1);
+			char current = charsStr[i];
+			charsStr[index] = charsStr[i];
+			charsStr[index] = current;
+		}
+		return new String(charsStr);
 	}
 }
